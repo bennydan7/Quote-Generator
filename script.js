ee5c51a9cca1cb09dfe6188cc6643aa443065306
bennydan7 <owusub243@gmail.com>
2023-10-31 13:51:13 +0000

    updatd codes

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
 // let apiQuotes = [];
+
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 //Show the new quote
 function newQuote() {
+  loading();
   // Pick a random quote from api quotes
   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-  console.log(quote);
+
+  //Check if author field is blank and replace it with quote unkown
+
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // Check Quote length to determine the styling
+
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  // Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes from API
-/*
+
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
@@ -23,6 +63,13 @@ async function getQuotes() {
 
 // On Load
 getQuotes();
-*/
 
-newQuote();
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
